8ef380f143ff33eaa7bb3cf6a065c650cb9c31fb
jeongabae <gabea52@gmail.com>
2024-04-30 14:54:28 +0900

    Update: 깃 연결

---
+++ src/test/java/com/example/rentitbackend/RentItBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rentitbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentItBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
